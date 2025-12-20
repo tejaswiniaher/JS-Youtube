fc3bb868968ef33811099eb09a01bf07385ae094
tejaswiniaher <tejaswiniaher905@gmail.com>
2025-12-20 03:35:15 +0000

    datatype summarry

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,6 +24,44 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 // Number to String conversion
 let someNumber = 333
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *******************************Operation***********************
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " hitesh"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+console.log(+true);
+console.log(+"");
+
+
+gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
 
---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,38 @@
+//  Primitives
+
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+//Example:
+const score = 100;
+const scorevalue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherid = Symbol('123')
+
+console.log(id === anotherid);
+
+const bigNumber = 432554225543439475544344687873468944365n
+
+
+// reference (Non premitive):
+// Array, Object, Function
+
+const heros = ["shaktima", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: "24",
+}
+
+
+const MyFunction = function() {
+    console.log("Hello World!");    
+}
+
+console.log(typeof bigNumber);
+console.log(typeof MyFunction);
+console.log(typeof anotherid);
+console.log(typeof heros);
