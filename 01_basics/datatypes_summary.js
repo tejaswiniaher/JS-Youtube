aa206187a2eb5ece1ead04f2f15c616dfeb5a178
tejaswiniaher <tejaswiniaher905@gmail.com>
2025-12-20 08:40:18 +0000

    stack and heap

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -13,7 +13,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherid = Symbol('123')
 
-console.log(id === anotherid);
+// console.log(id === anotherid);
 
 const bigNumber = 432554225543439475544344687873468944365n
 
@@ -32,7 +32,29 @@ const MyFunction = function() {
     console.log("Hello World!");    
 }
 
-console.log(typeof bigNumber);
-console.log(typeof MyFunction);
-console.log(typeof anotherid);
-console.log(typeof heros);
+// console.log(typeof bigNumber);
+// console.log(typeof MyFunction);
+// console.log(typeof anotherid);
+// console.log(typeof heros);
+
+// ********************************************************
+// Stack(Premitives) && Heap(Non-Premitives)
+
+let myYoutubename = "hiteshchoudharydotcom"
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ypl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "hitesh@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
