600164557e4eb4dcc626b3a2a898fe7fbe57eaa7
tejaswiniaher <tejaswiniaher905@gmail.com>
2025-12-21 15:20:45 +0000

    loop

---
+++ 05_iteration/one.js
@@ -0,0 +1,22 @@
+// for
+
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    if(element == 5) {
+        // console.log("5 is best number");
+        
+    }
+    // console.log(element);
+}
+
+
+for (let i = 0; i < 5; i++) {
+    console.log(`outer loop value: ${i}`);
+    
+    for (let j = 0; j < 5; j++) {
+        // console.log(`Inner loop value ${j} and inner loop ${i}`);
+        
+        
+    }
+    
+}
