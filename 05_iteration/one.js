db365f44f7b812137d26fc7e8801156e6356b57d
tejaswiniaher <tejaswiniaher905@gmail.com>
2025-12-22 03:17:38 +0000

    for loop done

--- 05_iteration/one.js
+++ 05_iteration/one.js
@@ -10,13 +10,46 @@ for (let i = 0; i < 10; i++) {
 }
 
 
-for (let i = 0; i < 5; i++) {
-    console.log(`outer loop value: ${i}`);
+
+for (let i = 1; i < 5; i++) {
+    // console.log(`outer loop value: ${i}`);
     
-    for (let j = 0; j < 5; j++) {
+    for (let j = 1; j < 5; j++) {
         // console.log(`Inner loop value ${j} and inner loop ${i}`);
+        // console.log(i + "*" + j + " = " + i*j);
         
         
     }
     
+}
+
+
+let myArray = ["flash", "batmaan", "superman"]
+// console.log(myArray.length);
+
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    // console.log(element);
+    
+}
+
+
+// break and continue
+// for (let index = 0; index <= 20; index++) {
+//     if(index == 5){
+//         console.log('Detected 5');
+//         break
+//     }
+//     console.log(`Value of i is ${index}`);
+// }
+
+
+
+for (let index = 0; index <= 20; index++) {
+    if(index == 5){
+        console.log('Detected 5');
+        continue
+    }
+    console.log(`Value of i is ${index}`);
 }
